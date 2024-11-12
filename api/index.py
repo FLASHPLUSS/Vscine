44522281daf8ef6f3bfe69688bc6aaf91f69cd29
DEVELOPS MAX <132115571+FLASHPLUSS@users.noreply.github.com>
2024-11-12 12:55:20 -0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -12,7 +12,7 @@ def pesquisa():
     if not titulo:
         return jsonify({"erro": "Título do filme não fornecido!"}), 400
 
-    # URL de pesquisa no site para encontrar o filme
+    # URL de pesquisa no site
     url_busca = f"https://assistir.biz/busca?q={titulo}"
 
     # Cabeçalhos para evitar bloqueios (definindo o User-Agent)
@@ -28,29 +28,11 @@ def pesquisa():
         # Usando BeautifulSoup para fazer o parsing do HTML da página de busca
         soup = BeautifulSoup(response.text, 'html.parser')
 
-        # Encontrando o link da página do filme, que está no href do <a>
+        # Encontrando o link da página do filme, que está no href do <a> com a classe "card__play"
         link_pagina_filme = soup.find("a", class_="card__play")
         if link_pagina_filme:
             filme_url = "https://assistir.biz" + link_pagina_filme['href']
-
-            # Fazendo uma nova requisição para a página do filme
-            response_filme = requests.get(filme_url, headers=headers)
-            response_filme.raise_for_status()
-
-            # Fazendo o parsing do HTML da página do filme
-            soup_filme = BeautifulSoup(response_filme.text, 'html.parser')
-
-            # Procurando pelo iframe que contém o link de reprodução
-            iframe = soup_filme.find("iframe")
-            if iframe:
-                link_reproducao = iframe['src']
-                # Verificando se o link não possui o prefixo http:// ou https://
-                if not link_reproducao.startswith('http://') and not link_reproducao.startswith('https://'):
-                    link_reproducao = 'http:' + link_reproducao
-                return jsonify({"link": link_reproducao}), 200
-
-            # Caso o link de reprodução não seja encontrado na página do filme
-            return jsonify({"erro": "Link de reprodução não encontrado na página do filme!"}), 404
+            return jsonify({"link": filme_url}), 200
 
         # Caso o filme não seja encontrado na busca
         return jsonify({"erro": "Filme não encontrado!"}), 404
